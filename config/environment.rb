c958dd5664471b3bbfba4b887dabbc4025ae88aa
Jeff <jeffgichuki68@gmail.com>
2023-02-22 14:34:06 +0300

    completed

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,15 @@
 require 'bundler'
 Bundler.require
-
 require_relative '../lib/dog'
 
+
 DB = { conn: SQLite3::Database.new("db/dogs.db") }
+
+
+Dog.create_table
+
+dog1 =Dog.new(name: "Joe", breed: "Corgi")
+
+puts dog1
+
+pp dog1.name
